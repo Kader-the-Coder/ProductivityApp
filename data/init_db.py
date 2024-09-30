31cf0482d01e608c4eff62b608a93ac4f63f72f3
Abdul Kader <thekader786@gmail.com>
2024-09-30 18:10:30 +0200

    Refactored main and added database setup options

---
+++ data/init_db.py
@@ -0,0 +1,93 @@
+import sqlite3
+import sys
+
+def init_db(database: str = "data/db.sqlite3"):
+    """Initialize the database."""
+
+    sql_script = '''
+    /* Table for categories */
+    CREATE TABLE IF NOT EXISTS category (
+        category_id INTEGER PRIMARY KEY,
+        category_name VARCHAR(20) UNIQUE NOT NULL
+    );
+
+    /* Table for templates */
+    CREATE TABLE IF NOT EXISTS templates (
+        template_id INTEGER PRIMARY KEY,
+        template_name VARCHAR(20) UNIQUE NOT NULL,
+        template_text TEXT NOT NULL,
+        category_id INT, -- Foreign key to category table
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (category_id) REFERENCES category (category_id) 
+        ON DELETE SET NULL -- Temporarily set to NULL on delete
+    );
+
+    /* Trigger to move templates to 'Unassigned' category if category is deleted */
+    CREATE TRIGGER IF NOT EXISTS move_templates_to_unassigned
+    AFTER UPDATE ON templates
+    FOR EACH ROW
+    WHEN NEW.category_id IS NULL
+    BEGIN
+        UPDATE templates 
+        SET category_id = (SELECT category_id FROM category WHERE category_name = 'Unassigned')
+        WHERE template_id = NEW.template_id;
+    END;
+
+    /* Table for tags */
+    CREATE TABLE IF NOT EXISTS tags (
+        tag_id INTEGER PRIMARY KEY,
+        tag_name VARCHAR(255) UNIQUE NOT NULL
+    );
+
+    /* Table to associate templates with tags (many-to-many relationship) */
+    CREATE TABLE IF NOT EXISTS template_tags (
+        template_id INT NOT NULL,
+        tag_id INT NOT NULL,
+        PRIMARY KEY (template_id, tag_id), -- Composite key
+        FOREIGN KEY (template_id) REFERENCES templates (template_id) ON DELETE CASCADE,
+        FOREIGN KEY (tag_id) REFERENCES tags (tag_id) ON DELETE CASCADE
+    );
+
+    /* Table for quick copy buttons */
+    CREATE TABLE IF NOT EXISTS quick_copy_buttons (
+        button_id INTEGER PRIMARY KEY,
+        button_icon VARCHAR(1) NOT NULL,
+        button_text VARCHAR(20) NOT NULL
+    );
+
+    /* Insert dummy data into category table */
+    INSERT OR IGNORE INTO category (category_name) VALUES
+        ('Unassigned'),
+        ('Completeness'),
+        ('Efficiency'),
+        ('Style'),
+        ('Documentation'),
+        ('Links'),
+        ('Other');
+
+    /* Insert dummy data into quick_copy_buttons table */
+    INSERT OR IGNORE INTO quick_copy_buttons (button_icon, button_text) VALUES
+        ('Space', '‎ ‎ ‎ ‎ '),
+        ('❌', '❌ '),
+        ('●', '●'),
+        ('○', '○'),
+        ('▪', '▪'),
+        ('→', '→'),
+        ('✔️', '✔️ '),
+        ('⚠️', '⚠️'),
+        ('🔎', '🔎'),
+        ('📖', '📖'),
+        ('🔗', '🔗'),
+        ('💡', '💡'),
+        ('⬆️', '⬆️');
+    '''
+
+    # Connect to the SQLite database and execute the SQL script
+    try:
+        with sqlite3.connect(database) as conn:
+            cursor = conn.cursor()
+            cursor.executescript(sql_script)
+            conn.commit()
+            print("Database initialized successfully.")
+    except sqlite3.Error as e:
+        print(f"An error occurred while initializing the database: {e}")
--- main.py
+++ main.py
@@ -1,8 +1,30 @@
 """Main entry point of the application"""
+import sys
+import os
 import tkinter as tk
+from data.init_db import init_db
 from frames.frames_main import TemplatePro
 
+# Set working directory
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+DATABASE_PATH = "data/db.sqlite3"
+
 if __name__ == "__main__":
-    main_root = tk.Tk()
-    app = TemplatePro(main_root)
-    main_root.mainloop()
+    if len(sys.argv) > 1:
+        # Create and initialize database.
+        if sys.argv[1] == 'init_db':
+            init_db()
+        else:
+            print("Usage: python main.py init_db")
+    else:
+        # Check if the database file exists before running the main loop
+        if os.path.exists(DATABASE_PATH):
+            # Main loop
+            main_root = tk.Tk()
+            app = TemplatePro(main_root)
+            main_root.mainloop()
+        else:
+            print(f"Database file '{DATABASE_PATH}' does not exist. "
+                  "Please initialize the database first by running:")
+            print("python main.py init_db")
